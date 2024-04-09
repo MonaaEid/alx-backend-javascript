2d1b3be98e607a51554e90ff62325cf16f50fb89
MonaaEid <monaa_eid@hotmail.com>
2024-04-09 23:00:43 +0200

    update

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 export default function getResponseFromAPI () {
-  return new Promise((resolve, reject) => {
-    if (true) {
-      resolve();
-    } else {
-      reject();
-    }
+  return new Promise(() => {
   }
   );
 }
