8d0127440c51e4686d068352dcc0c688bb158b6c
MonaaEid <monaa_eid@hotmail.com>
2024-04-10 01:41:42 +0200

    test

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default function handleResponseFromAPI(promise) {
+  return promise.then(() => ({
+    status: 200,
+    body: 'success',
+  })).catch(() => Error()).finally(() => {
+    console.log('Got a response from the API');
+  });
+}
+const promise = Promise.resolve();
+handleResponseFromAPI(promise);
