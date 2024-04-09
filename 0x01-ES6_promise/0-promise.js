2b1d0a87123e672885acf43cfab53a65384e6afb
MonaaEid <monaa_eid@hotmail.com>
2024-04-09 21:41:54 +0200

    add readme file

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            resolve();
+        } else {
+            reject();
+        }
+    }
+    );
+}
+
+// const response = getResponseFromAPI();
+// console.log(response instanceof Promise);
+
