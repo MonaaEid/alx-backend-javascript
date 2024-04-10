2af3003ef99999e7b28243ea21b63e91417a1a67
MonaaEid <monaa_eid@hotmail.com>
2024-04-10 03:19:38 +0200

    Add 100-await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,17 @@
+import { uploadPhoto, createUser } from './utils';
+
+async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    console.error(error);
+    return { photo: null, user: null };
+  }
+}
+
+asyncUploadUser().then(console.log).catch(console.error);
