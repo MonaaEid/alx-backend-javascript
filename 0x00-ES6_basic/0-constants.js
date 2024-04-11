e09b951798d9de17ebbc012beb87514010b614ad
MonaaEid <monaa_eid@hotmail.com>
2024-04-11 04:41:45 +0200

    test

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,17 +1,15 @@
-let taskFirst = function taskFirst() {
-    var task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-var taskNext = function taskNext() {
-    var combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
 
-export { taskFirst, taskNext };
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
