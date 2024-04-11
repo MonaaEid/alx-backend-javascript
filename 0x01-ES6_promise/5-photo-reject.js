e5a815bd00132b36c8abb4808bf4ab66a771a18c
MonaaEid <monaa_eid@hotmail.com>
2024-04-11 03:46:06 +0200

    Add 100-await

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,4 @@
 export default function uploadPhoto(filename) {
-    return promise.reject(new Error(`${filename} cannot be processed`));
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
+console.log(uploadPhoto('guillaume.jpg'));
