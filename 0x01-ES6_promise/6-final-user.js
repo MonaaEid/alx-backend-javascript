955e07f020ef998d716661a1ca6db6b240d552ac
MonaaEid <monaa_eid@hotmail.com>
2024-04-11 03:50:54 +0200

    test

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    return Promise.allSettled([import('./4-user-promise.js'), import('./5-photo-reject.js')])
+        .then(([{ signUpUser }, { default: uploadPhoto }]) => {
+        signUpUser(firstName, lastName).then((value) => {
+            uploadPhoto(fileName).then(() => {
+            console.log(value);
+            console.log('Photo upload completed');
+            });
+        });
+        });
+    }
