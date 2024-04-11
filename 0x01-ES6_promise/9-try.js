9fc675d1b9bfbed3cf1e7ca751a6052fd269c690
MonaaEid <monaa_eid@hotmail.com>
2024-04-11 04:03:21 +0200

    test

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(error.toString());
+  }
+  return queue;
+}
