d8f46271bd1ef5781f908c9f789e0fb4fc646e52
MonaaEid <monaa_eid@hotmail.com>
2024-04-11 04:30:07 +0200

    test

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,16 +1,26 @@
 import { uploadPhoto, createUser } from './utils';
 
 async function asyncUploadUser() {
+  let result;
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
-    console.log({
-        photo: photo.body,
-        user: `${user.firstName} ${user.lastName}`
-    });
-    }
-    catch (error) {
-        console.log({photo: null, user: null});
-    }
+    result = {
+      photo,
+      user,
+    };
+  } catch (error) {
+    result = {
+      photo: null,
+      user: null,
+    };
+  }
+  return result;
 }
 
+const test = async () => {
+    const value = await asyncUploadUser();
+    console.log(value);
+};
+
+test();
