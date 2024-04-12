ee20cacacefcc2464875a2af1e965813194da443
MonaaEid <monaa_eid@hotmail.com>
2024-04-12 21:59:31 +0200

    update

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  for (const idx of array) {
+    const value = array.indexOf(idx);
+    array[idx] = appendString + value;
+  }
+
+  return array;
+}
