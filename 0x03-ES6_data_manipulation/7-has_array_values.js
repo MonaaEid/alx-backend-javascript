87e1a75547307879a98b08aec340d1b22af46fda
MonaaEid <monaa_eid@hotmail.com>
2024-04-17 01:43:56 +0200

    update

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+export default function hasValuesFromArray(set, array) {
+  return array.every((value) => set.has(value));
+}
+
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10]));
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+export default function cleanSet(set, startString) {
+  const newSet = new Set([...set].map((value) => {
+    if (value.startsWith(startString) && typeof value === 'string') {
+      return value.slice(startString.length);
+    }
+    return '';
+  }));
+  return Array.from(newSet).join('-');
+}
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
