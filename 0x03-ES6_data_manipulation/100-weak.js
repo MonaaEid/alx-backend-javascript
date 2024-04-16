5eb92ca11f1a34b09d19a9ad99df533422f0ef1b
MonaaEid <monaa_eid@hotmail.com>
2024-04-17 01:57:00 +0200

    update

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ export const weakMap = new WeakSet();
 
 export default function queryAPI(endpoint) {
     weakMap.add(endpoint);
-    if (weakMap.size >= 5) {
+    if (weakMap.queryAPI >= 5) {
         throw new Error('Endpoint load is high');
     }
     return weakMap;
