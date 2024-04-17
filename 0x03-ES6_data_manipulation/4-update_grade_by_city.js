a2b37c5f9d9497d13add2bac24a938bcf751e1bc
MonaaEid <monaa_eid@hotmail.com>
2024-04-17 04:08:20 +0200

    update

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,14 @@
 function updateStudentGradeByCity(students, city, newGrades) {
-  return students.map((student) => {
-    if (student.city === city) {
-      return {
-        ...student,
-        grades: newGrades,
-      };
+  const arrByCity = students.filter((item) => item.location === city);
+  const arrByGrade = arrByCity.map((item) => {
+    const output = item;
+    const grade = newGrades.find((grade) => grade.studentId === item.id);
+    if (grade) {
+      output.grade = grade.grade;
+    } else {
+      output.grade = 'N/A';
     }
-    return student;
+    return output;
   });
+  return arrByGrade;
 }
