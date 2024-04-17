14166e51399863327f9eb2f0e1df7601b5b90d16
MonaaEid <monaa_eid@hotmail.com>
2024-04-17 04:10:49 +0200

    update

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,8 @@
 export default function cleanSet(set, startString) {
-  const newSet = new Set([...set].map((value) => {
-    if (value.startsWith(startString) && typeof value === 'string') {
-      return value.slice(startString.length);
-    }
-    return '';
-  }));
-  return Array.from(newSet).join('-');
+    const newSet = [...set].filter(value => value.startsWith(startString));
+    return newSet.join("-");
 }
+
 
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
