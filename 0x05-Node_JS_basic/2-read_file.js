78c0b82652c2410fbc4812098e29cc0ae2db19a8
MonaaEid <monaa_eid@hotmail.com>
2024-06-04 23:50:01 +0300

    test

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,1 +1,25 @@
-#!/usr/bin/node
+function countStudents(path) {
+    try {
+        const data = fs.readFileSync(path, 'utf8').split('\n');
+        let students = data.slice(1, data.length - 1);
+        let studentsByField = {};
+        for (let i = 0; i < students.length; i++) {
+            let student = students[i].split(',');
+            if (!studentsByField[student[3]]) {
+                studentsByField[student[3]] = [];
+            }
+            studentsByField[student[3]].push(student[0]);
+        }
+        console.log(`Number of students: ${students.length}`);
+        for (const field in studentsByField) {
+            if (field) {
+                console.log(`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
+            }
+        }
+    }
+    catch (err) {
+        throw new Error('Cannot load the database');
+    }
+}
+
+module.exports = countStudents;
