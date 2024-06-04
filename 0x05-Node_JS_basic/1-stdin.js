7307c405ff87e84a0b9e257e17a769cb8383d988
MonaaEid <monaa_eid@hotmail.com>
2024-06-05 00:17:10 +0300

    test

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,11 +8,11 @@ const rl = readline.createInterface({
 console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
+  rl.stdout.write(`Your name is: ${input}`);
 });
 
 rl.on('close', () => {
-  console.log('This important software is now closing');
+  rl.stdout.write('This important software is now closing\n');
 });
 
 module.exports = rl;
