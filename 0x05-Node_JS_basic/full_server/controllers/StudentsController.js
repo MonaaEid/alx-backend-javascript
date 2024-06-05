58e2b3c6d55d38063227ae035f1df8cd08e0a575
MonaaEid <monaa_eid@hotmail.com>
2024-06-05 05:51:35 +0300

    test

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,41 +1,33 @@
 const readDatabase = require('../utils');
 
 class StudentsController {
-  static getAllStudents(request, response) {
-    readDatabase(process.argv[2])
-      .then((data) => {
-        const msg = 'This is the list of our students\n';
-        const students = [];
-        for (const [key, value] of Object.entries(data)) {
-          students.push(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+    static getAllStudents(request, response) {
+      readDatabase(process.argv[2].toString()).then((students) => {
+        const output = [];
+        output.push('This is the list of our students');
+        const keys = Object.keys(students);
+        keys.sort();
+        for (let i = 0; i < keys.length; i += 1) {
+          output.push(`Number of students in ${keys[i]}: ${students[keys[i]].length}. List: ${students[keys[i]].join(', ')}`);
         }
-        response.write(msg);
-        response.write(students.join('\n'));
-        response.end();
-      })
-      .catch((error) => {
-        response.write('This is the list of our students\n');
-        response.end(error.message);
+        response.status(200).send(output.join('\n'));
+      }).catch(() => {
+        response.status(500).send('Cannot load the database');
       });
-  }
-
-  static getAllStudentsByMajor(request, response) {
-    readDatabase(process.argv[2])
-      .then((data) => {
-        const students = data[request.params.major];
-        if (students) {
-          response.write(`List of students in ${request.params.major}: ${students.join(', ')}`);
-          response.end();
+    }
+  
+    static getAllStudentsByMajor(request, response) {
+      const field = request.params.major;
+      readDatabase(process.argv[2].toString()).then((students) => {
+        if (!(field in students)) {
+          response.status(500).send('Major parameter must be CS or SWE');
         } else {
-          response.write('Major parameter must be CS or SWE\n');
-          response.end();
+          response.status(200).send(`List: ${students[field].join(', ')}`);
         }
-      })
-      .catch((error) => {
-        response.write('This is the list of our students\n');
-        response.end(error.message);
+      }).catch(() => {
+        response.status(500).send('Cannot load the database');
       });
+    }
   }
-}
 
 module.exports = StudentsController;
