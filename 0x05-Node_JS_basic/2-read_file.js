bb4625fd5ecf26e0a9f3e1c52636546e707d3350
MonaaEid <monaa_eid@hotmail.com>
2024-06-05 03:11:44 +0300

    test

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,25 +1,26 @@
+const fs = require('fs');
+
 function countStudents(path) {
-    try {
-        const data = fs.readFileSync(path, 'utf8').split('\n');
-        let students = data.slice(1, data.length - 1);
-        let studentsByField = {};
-        for (let i = 0; i < students.length; i++) {
-            let student = students[i].split(',');
-            if (!studentsByField[student[3]]) {
-                studentsByField[student[3]] = [];
-            }
-            studentsByField[student[3]].push(student[0]);
-        }
-        console.log(`Number of students: ${students.length}`);
-        for (const field in studentsByField) {
-            if (field) {
-                console.log(`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
-            }
-        }
+  try {
+    const data = fs.readFileSync(path, 'utf8').split('\n');
+    const students = data.slice(1, data.length - 1);
+    const studentsByField = {};
+    for (let i = 0; i < students.length; i++) {
+      const student = students[i].split(',');
+      if (!studentsByField[student[3]]) {
+        studentsByField[student[3]] = [];
+      }
+      studentsByField[student[3]].push(student[0]);
     }
-    catch (err) {
-        throw new Error('Cannot load the database');
+    console.log(`Number of students: ${students.length}`);
+    for (const field in studentsByField) {
+      if (field) {
+        console.log(`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
+      }
     }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,1 +1,29 @@
-#!/usr/bin/node
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        throw new Error('Cannot load the database');
+      }
+      const students = data.split('\n').slice(1).map((line) => line.split(','));
+      const studentsByField = {};
+      for (const student of students) {
+        if (!studentsByField[student[3]]) {
+          studentsByField[student[3]] = [];
+        }
+        studentsByField[student[3]].push(student[0]);
+      }
+      console.log(`Number of students: ${students.length}`);
+      for (const field in studentsByField) {
+        if (field) {
+          console.log(`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
+        }
+      }
+    });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
