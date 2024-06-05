fee6b1067f884cd926438b5b1dd717b2b23ee430
MonaaEid <monaa_eid@hotmail.com>
2024-06-05 04:36:34 +0300

    test

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,1 +1,41 @@
-#!/usr/bin/node
+const readDatabase = require('../utils');
+
+class StudentsController {
+  static getAllStudents(request, response) {
+    readDatabase(process.argv[2])
+      .then((data) => {
+        const msg = 'This is the list of our students\n';
+        const students = [];
+        for (const [key, value] of Object.entries(data)) {
+          students.push(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+        }
+        response.write(msg);
+        response.write(students.join('\n'));
+        response.end();
+      })
+      .catch((error) => {
+        response.write('This is the list of our students\n');
+        response.end(error.message);
+      });
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    readDatabase(process.argv[2])
+      .then((data) => {
+        const students = data[request.params.major];
+        if (students) {
+          response.write(`List of students in ${request.params.major}: ${students.join(', ')}`);
+          response.end();
+        } else {
+          response.write('Major parameter must be CS or SWE\n');
+          response.end();
+        }
+      })
+      .catch((error) => {
+        response.write('This is the list of our students\n');
+        response.end(error.message);
+      });
+  }
+}
+
+module.exports = StudentsController;
