6ca27ecbdca5587d367cc8a8152a39a2bea48903
MonaaEid <monaa_eid@hotmail.com>
2024-06-07 01:20:34 +0300

    test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,30 +2,56 @@ const assert = require("assert");
 const { it, describe } = require("mocha");
 const calculateNumber = require("./0-calcul");
 
-describe("calculateNumber()", function() {
-
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber(1, 2);
-      assert.strictEqual(res, 3);
+describe("calculateNumber", function () {
+    it("should return 4", function () {
+        assert.strictEqual(calculateNumber(1, 3), 4);
     });
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber(1.4, 2.2);
-      assert.strictEqual(res, 3);
+    it("should return 5", function () {
+        assert.strictEqual(calculateNumber(1.9, 3), 5);
     });
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber(1.6, 2.7);
-      assert.strictEqual(res, 5);
+    it("should return 5", function () {
+        assert.strictEqual(calculateNumber(1, 3.9), 5);
     });
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber(0, 0);
-      assert.strictEqual(res, 0);
+    it("should return 5", function () {
+        assert.strictEqual(calculateNumber(1.4, 3.5), 5);
     });
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber(-1.6, -1.7);
-      assert.strictEqual(res, -4);
+    it("should return 6", function () {
+        assert.strictEqual(calculateNumber(1.4, 3.5, 'ROUND_DOWN'), 4);
     });
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber(-1.4, -1.3);
-	assert.strictEqual(res, -2);
+    it("should return 5", function () {
+        assert.strictEqual(calculateNumber(1.5, 3.5, 'ROUND_UP'), 5);
+    });
+    it("should return 6", function () {
+        assert.strictEqual(calculateNumber(1.6, 3.5, 'ROUND_UP'), 6);
+    });
+    it("should return 6", function () {
+        assert.strictEqual(calculateNumber(1.4, 3.5, 'ROUND_HALF_DOWN'), 5);
+    });
+    it("should return 5", function () {
+        assert.strictEqual(calculateNumber(1.5, 3.5, 'ROUND_HALF_UP'), 5);
+    });
+    it("should return 5", function () {
+        assert.strictEqual(calculateNumber(1.6, 3.5, 'ROUND_HALF_UP'), 5);
+    });
+    it("should return 5", function () {
+        assert.strictEqual(calculateNumber(1.4, 3.5, 'ROUND_HALF_EVEN'), 5);
+    });
+    it("should return 6", function () {
+        assert.strictEqual(calculateNumber(1.5, 3.5, 'ROUND_HALF_EVEN'), 6);
+    });
+    it("should return 6", function () {
+        assert.strictEqual(calculateNumber(1.6, 3.5, 'ROUND_HALF_EVEN'), 6);
+    });
+    it("should return 6", function () {
+        assert.strictEqual(calculateNumber(1.2, 3.7, 'ROUND_HALF_EVEN'), 5);
+    });
+    it("should return 5", function () {
+        assert.strictEqual(calculateNumber(1.5, 3.5, 'ROUND_HALF_DOWN'), 5);
+    });
+    it("should return 5", function () {
+        assert.strictEqual(calculateNumber(1.6, 3.5, 'ROUND_HALF_DOWN'), 5);
+    });
+    it("should return 5", function () {
+        assert.strictEqual(calculateNumber(1.4, 3.5, 'ROUND_HALF_UP'), 5);
     });
 });
